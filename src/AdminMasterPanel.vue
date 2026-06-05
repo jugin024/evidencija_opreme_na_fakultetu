c48cdf2e3e7282a4e61814acbb8d3298680b9aa3
eugenb024 <ebucko@student.unipu.hr>
2026-06-05 22:50:47 +0200

    prikaz svih podataka iz baze na admin panelu + UI overhaul

--- src/AdminMasterPanel.vue
+++ src/AdminMasterPanel.vue
@@ -1,24 +1,44 @@
 <template>
   <div>
     <h2 class="mb-4">Admin Panel - Pristigli zahtjevi</h2>
-    <div class="table-responsive">
-      <table class="table table-hover table-bordered bg-white shadow-sm">
-        <thead class="thead-dark">
+    <div class="d-flex justify-content-center">
+      <table
+        class="table table-hover table-bordered bg-white shadow-sm text-center"
+      >
+        <thead class="thead-dark" style="text-align: center">
           <tr>
             <th>Korisnik</th>
             <th>Oprema</th>
             <th>Od - Do</th>
+            <th>Sati</th>
             <th>Status</th>
             <th>Akcija</th>
+            <th>Datum zahtjeva</th>
           </tr>
         </thead>
 
-        <tbody>
+        <tbody class="align-middle">
           <tr v-for="zahtjev in zahtjevi" :key="zahtjev.id">
             <td>{{ zahtjev.korisnikEmail }}</td>
-            <td>{{ zahtjev.oprema }}</td>
-            <td>{{ zahtjev.datumOd }} - {{ zahtjev.datumDo }}</td>
-
+            <td style="vertical-align: middle; width: 350px; min-width: 300px">
+              <div
+                style="
+                  display: -webkit-box;
+                  -webkit-line-clamp: 2;
+                  -webkit-box-orient: vertical;
+                  overflow: hidden;
+                  text-overflow: ellipsis;
+                "
+              >
+                {{ zahtjev.oprema }}
+              </div>
+            </td>
+            <td>
+              {{ formatDatuma(zahtjev.datumOd, zahtjev.datumDo) }}
+            </td>
+            <td style="white-space: nowrap">
+              {{ zahtjev.vrijemeOd }} - {{ zahtjev.vrijemeDo }}
+            </td>
             <td>
               <span
                 v-if="zahtjev.status === 'na_cekanju'"
@@ -38,21 +58,29 @@
             </td>
 
             <td>
-              <div v-if="zahtjev.status === 'na_cekanju'">
+              <div
+                v-if="zahtjev.status === 'na_cekanju'"
+                style="display: flex; justify-content: center; gap: 10px"
+              >
                 <button
-                  class="btn btn-sm btn-success mr-2"
+                  class="btn btn-success"
+                  style="width: 100px; font-weight: bold"
                   @click="promijeniStatus(zahtjev.id, 'odobreno')"
                 >
                   Odobri
                 </button>
                 <button
-                  class="btn btn-sm btn-danger"
+                  class="btn btn-danger"
+                  style="width: 100px; font-weight: bold"
                   @click="promijeniStatus(zahtjev.id, 'odbijeno')"
                 >
                   Odbij
                 </button>
               </div>
-              <div v-else class="text-muted font-italic">Riješeno</div>
+              <div v-else style="white-space: nowrap">Riješeno</div>
+            </td>
+            <td class="white-space: nowrap; text-nowrap">
+              {{ formatTimestamp(zahtjev.kreirano) }}
             </td>
           </tr>
         </tbody>
@@ -89,15 +117,40 @@ export default {
               datumOd: document.data().datumOd,
               datumDo: document.data().datumDo,
               status: document.data().status,
+              vrijemeOd: document.data().vrijemeOd,
+              vrijemeDo: document.data().vrijemeDo,
+              kreirano: document.data().kreirano,
             });
           });
 
           this.zahtjevi = zahtjeviUnutarBaze;
         });
     },
+    formatDatuma(prvi_D, drugi_D) {
+      let datum_prvi = prvi_D.split("-");
+      let datum_Drugi = drugi_D.split("-");
+      return (
+        datum_prvi[2] +
+        ".-" +
+        datum_Drugi[2] +
+        "." +
+        datum_Drugi[1] +
+        "." +
+        datum_Drugi[0] +
+        "."
+      );
+    },
+
+    formatTimestamp(iso) {
+      // "2026-06-04T15:07:42.488Z"
+      let datum = iso.slice(0, 10);
+      let vrijeme = iso.slice(11, 19);
+      let d = datum.split("-");
+
+      return vrijeme + "-" + d[2] + "." + d[1] + "." + d[0] + ".";
+    },
     promijeniStatus(id, noviStatus) {
       db.collection("rezervacije").doc(id).update({ status: noviStatus });
-
       const pronadjeniZahtjev = this.zahtjevi.find((z) => z.id === id);
       if (pronadjeniZahtjev) {
         pronadjeniZahtjev.status = noviStatus;
@@ -108,6 +161,10 @@ export default {
 </script>
 <style scoped>
 .table {
-  font-size: 20px;
+  font-size: 18px;
+}
+.btn-fiksno {
+  width: 80px;
+  font-weight: bold;
 }
 </style>
